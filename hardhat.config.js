44afa9d6e6da6e08421f2acf33e292932acfaae8
big.wampa <79389347+bigwampa@users.noreply.github.com>
2021-08-18 12:00:43 -0700

    comment out optional lines

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 module.exports = {
   networks: {
     hardhat: {
-      accounts: [{privateKey: process.env.PRIVATE_KEY, balance: "100000000000000000000"}],
+      // accounts: [{privateKey: process.env.PRIVATE_KEY, balance: "100000000000000000000"}],
       chainId: 43114,
       forking: {
         url: "https://api.avax.network/ext/bc/C/rpc",
@@ -14,11 +14,11 @@ module.exports = {
     },
     fuji: {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
-      accounts: [process.env.PRIVATE_KEY]
+      // accounts: [process.env.PRIVATE_KEY]
     },
     mainnet: {
       url: "https://api.avax.network/ext/bc/C/rpc",
-      accounts: [process.env.PRIVATE_KEY]
+      // accounts: [process.env.PRIVATE_KEY]
     }
   },
   solidity: {
